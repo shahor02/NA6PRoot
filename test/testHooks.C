8a9d05a2fe9111e8aaf9b1732301339243157d27
rarnaldi <roberta.arnaldi@cern.ch>
2025-10-20 15:15:55 +0200

    add JPsi, Phi and Omega decay in dimuons and add a macro to generate JPsi, Omega and Phi over background

--- test/testHooks.C
+++ test/testHooks.C
@@ -8,6 +8,29 @@ int testHooks(int arg, bool inout)
 {
   if (arg == UserHook::ADDParticles) {
     LOGP(info, "Calling user hook from {} of the AddParticles method", inout ? "entry" : "exit");
+    {
+      LOGP(info, "Restricting J/psi decays to dimuon channel");
+      int Jpsi_decay_mode[6][3] = {{0}};
+      float Jpsi_BR[6] = {100.f, 0.f, 0.f, 0.f, 0.f, 0.f};
+      Jpsi_decay_mode[0][0] = 13;  // mu-
+      Jpsi_decay_mode[0][1] = -13; // mu+
+      TVirtualMC::GetMC()->SetDecayMode(443, Jpsi_BR, Jpsi_decay_mode);
+      
+      LOGP(info, "Restricting Phi decays to dimuon channel");
+      int Phi_decay_mode[6][3] = {{0}};
+      float Phi_BR[6] = {100.f, 0.f, 0.f, 0.f, 0.f, 0.f};
+      Phi_decay_mode[0][0] = 13;  // mu-
+      Phi_decay_mode[0][1] = -13; // mu+
+      TVirtualMC::GetMC()->SetDecayMode(333, Phi_BR, Phi_decay_mode);
+
+      LOGP(info, "Restricting omega2body decays to dimuon channel");
+      int Omega_decay_mode[6][3] = {{0}};
+      float Omega_BR[6] = {100.f, 0.f, 0.f, 0.f, 0.f, 0.f};
+      Omega_decay_mode[0][0] = 13;  // mu-
+      Omega_decay_mode[0][1] = -13; // mu+
+      TVirtualMC::GetMC()->SetDecayMode(223, Omega_BR, Omega_decay_mode);
+    }
+
   } else if (arg == UserHook::SelectParticles) {
     if (!inout) {
       return 0; // do nothing
