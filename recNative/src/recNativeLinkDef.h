3e5cd4f8652dce633c67bdb2c7f42cf455866ba1
shahoian <ruben.shahoyan@cern.ch>
2025-12-03 00:50:53 +0100

    Add natice track and track_with_covariance classes

---
+++ recNative/src/recNativeLinkDef.h
@@ -0,0 +1,14 @@
+// NA6PCCopyright
+
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class NA6PTrackPar + ;
+#pragma link C++ class NA6PTrackParCov + ;
+#pragma link C++ class std::vector < NA6PTrackPar> + ;
+#pragma link C++ class std::vector < NA6PTrackParCov> + ;
+
+#endif
