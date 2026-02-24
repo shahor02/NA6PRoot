fed87d3c7cedc1c3dabecbcba51ed1db4cdf800d
scompar <enrico.scomparin@cern.ch>
2026-02-24 15:38:52 +0100

    Add setting of custom random seed when running PYTHIA (useful for parallel mode)

--- varia/Pythia2HEPMC/runGenPYTHIA.sh
+++ varia/Pythia2HEPMC/runGenPYTHIA.sh
@@ -12,10 +12,10 @@ MAXRAP=4.5
 CONFIG=PbPb_MB.cmnd
 OUTDIR=data/PYTHIA_PbPb_MB
 
-SEEDSTART=0
-SEEDEND=$(($SEEDSTART + $NJOBS))
+SEEDSTART=1
+SEEDEND=$(($SEEDSTART + $NJOBS - 1))
 
 parallel -j $NCORES \
   'mkdir -p '"$OUTDIR"'/{1} && \
-   ./GenPYTHIA --events '"$NEVENTSPERJOB"' --energy '"$ENERGY"' --ymin '"$MINRAP"' --ymax '"$MAXRAP"' --cmnd '"$CONFIG"' --out '"$OUTDIR"'/{1} > '"$OUTDIR"'/{1}/log_sim_seed{1}.txt' \
+   ./GenPYTHIA --events '"$NEVENTSPERJOB"' --energy '"$ENERGY"' --ymin '"$MINRAP"' --ymax '"$MAXRAP"' --cmnd '"$CONFIG"' --out '"$OUTDIR"'/{1} --random {1} > '"$OUTDIR"'/{1}/log_sim_seed{1}.txt' \
   ::: $(seq $SEEDSTART $SEEDEND)
